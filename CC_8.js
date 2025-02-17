e2f9c5e6e536a27fe5ed81ab94cbd6a720e4ceff
reiddedeaux <rdedeaux@usf.edu>
2025-02-16 22:10:03 -0500

    Task 2 - Product Price After Discount

--- CC_8.js
+++ CC_8.js
@@ -6,3 +6,10 @@ function calculateSalary(baseSalary, bonus, taxRate){
 calculateSalary(5000, 500, 0.1); // Net Salary: $5000.00
 calculateSalary(7000, 1000, 0.15); // Net Salary: $6950.00
 
+// Task 2 Product Price After Dsicount
+const calculateDiscount = function(price, disocuntRate){
+    const finalPrice = price - (price * disocuntRate);
+    console.log(`Final Price: $${finalPrice.toFixed(2)}`)
+} // Created a function to determine the final price and logges it into the console
+calculateDiscount(100, 0.2); // Final Price: $80.00
+calculateDiscount(250, 0.15); // Final Price: $212.50
