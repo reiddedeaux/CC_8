0885d43b49c95b5b6025627cc0a3fd4dd996ae89
reiddedeaux <rdedeaux@usf.edu>
2025-02-16 22:22:28 -0500

    Task 3 - Service Fee Calculation

--- CC_8.js
+++ CC_8.js
@@ -13,3 +13,16 @@ const calculateDiscount = function(price, disocuntRate){
 } // Created a function to determine the final price and logges it into the console
 calculateDiscount(100, 0.2); // Final Price: $80.00
 calculateDiscount(250, 0.15); // Final Price: $212.50
+
+// Task 3 Service Fee Calculation
+const calculateServiceFee = (amount, serviceType) => {
+    let feeRate = 0;
+    if (serviceType === "Premium") feeRate = 0.15;
+    else if (serviceType === "Standard") feeRate = .10;
+    else if (serviceType === "Basic") feeRate = .05;
+    const fee = amount * feeRate;
+    console.log(`Service Fee: $${fee.toFixed(2)}`)
+}; // Created an arrow function that applies a fee
+calculateServiceFee(200, "Premium"); // Service Fee: $30.00
+calculateServiceFee(500, "Standard"); // Service Fee: $50.00
+
